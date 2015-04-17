42946acf21c38dfd69c377f8087c28569a0c3387
BrodyMacfarlane <macfarlanebrody@gmail.com>
2015-04-16 19:13:53 -0600

    swaggy tunes

--- js/itunesService.js
+++ js/itunesService.js
@@ -8,6 +8,27 @@ app.service('itunesService', function($http, $q){
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
   //Note that in the above line, artist is the parameter being passed in. 
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
-
-    //Code here
+  	var dfd = $q.defer();
+    this.getData = function(artist){
+    	$http ({
+    		method: 'JSONP',
+    		url: 'https:itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+    	}).then(function(response){
+    		var parsedResponse = response.data.results;
+        var songs = {};
+        console.log(parsedResponse)
+        for (i = 0; i < parsedResponse.length; i++){
+          songs[i] = {
+            AlbumArt: parsedResponse[i].artworkUrl100,
+            Track: parsedResponse[i].trackName,
+            Artist: parsedResponse[i].artistName,
+            Collection: parsedResponse.collectionName,
+            Play: parsedResponse[i].previewURL,
+            Type: parsedResponse[i].kind,
+            CollectionPrice: parsedResponse[i].collectionPrice
+          }
+        }dfd.resolve(songs);
+    	})
+    	return dfd.promise;
+    }
 });
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -10,6 +10,7 @@ app.controller('mainCtrl', function($scope, itunesService){
       sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
       columnDefs: [
         {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
+        {field: 'Track', displayName: 'Track'},
         {field: 'Artist', displayName: 'Artist'},
         {field: 'Collection', displayName: 'Collection'},
         {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
@@ -29,7 +30,13 @@ app.controller('mainCtrl', function($scope, itunesService){
   //*remember, that method should be expecting an artist name. The artist name is coming from the input box on index.html, head over there and check if that input box is tied to any specific model we could use.
   //Also note that that method should be retuning a promise, so you could use .then in this function.
     
-    //Code here
+    $scope.getSongData = function(){
+      itunesService.getData($scope.artist).then(function(data) {
+        // $scope.songs = results;
+        $scope.songData = data;
+      })
+    }
+
 
 
   //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result
